a74651793ceccb2f093420f3b183f528bf0f3f31
Paolo Savoldi <paooolino@gmail.com>
2016-05-12 16:13:46 +0200

    implementing tests

--- server/tests/DataEngineTest.php
+++ server/tests/DataEngineTest.php
@@ -12,11 +12,10 @@
 		function __construct() {
 			$this->db = new \CMS\DB(\CMS\DB_HOST, \CMS\DB_NAME, \CMS\DB_USER, \CMS\DB_PASS);
 			$this->de = new \CMS\DataEngine($this->db);		
-			$this->de->db->exec(file_get_contents("startup_database.sql"));	
 		}
 		
-		public static function setUpBeforeClass() {
-			//
+		public function setUp() {
+			$this->de->db->exec(file_get_contents("startup_database.sql"));	
 		}
 		
 		// The tests are public methods that are named test*
@@ -25,10 +24,38 @@
 			$this->assertEquals("1", 1);
 		}
 		
-		public function testItems() {
+		public function testFetchItems() {
+			$result = $this->de->fetchItems(0, "en");
+			$this->assertEquals(count($result["items"]), 4);
+			$this->assertEquals(count($result["parent"]), 0);
+			
+			$result = $this->de->fetchItems(3, "en");
+			$this->assertEquals(count($result["items"]), 2);
+			$this->assertEquals($result["parent"]["id"], 3);
 		}
 		
-		public static function tearDownAfterClass() {
+		public function testAddItem() {
+			$result = $this->de->fetchItems(0, "en");
+			$n_items_before = count($result["items"]);
+			
+			$this->de->addItem(0, "en");
+			$result = $this->de->fetchItems(0, "en");
+			$n_items_after = count($result["items"]);
+			
+			$this->assertEquals($n_items_after - $n_items_before, 1);
+		}
+		
+		public function testSaveItemField() {
+			$this->de->saveItemField(1, "name", "Test");
+			$this->de->saveItemField(1, "slug", "test");
+			
+			$result = $this->de->fetchItem(1);
+			
+			$this->assertEquals($result["item"]["name"], "Test");
+			$this->assertEquals($result["item"]["slug"], "test");
+		}
+		
+		public function tearDown() {
 			//
 		}
 		
