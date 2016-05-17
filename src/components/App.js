e61834330981283f952827ee535dd25c3dc1199d
Paolo Savoldi <paooolino@gmail.com>
2016-05-17 15:39:19 +0200

    navbar style

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,14 @@ import ActionInfo from 'material-ui/svg-icons/action/info';
 import Paper from 'material-ui/Paper';
 import Avatar from 'material-ui/Avatar';
 import FileFolder from 'material-ui/svg-icons/file/folder';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import Subheader from 'material-ui/Subheader';
+import TextField from 'material-ui/TextField';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib/index';
 import { connect } from 'react-redux';
 
@@ -40,18 +48,39 @@ class App extends Component {
                     </Col>
                 </Row>
                 <Row>
-                    <Col xs className="navbar">
-                        <Paper zDepth={1} style={{height: (this.props.windowHeight - 64) + 'px'}}>
+                    <div className="navbar" style={{height: (this.props.windowHeight) + 'px'}}>
+                        <Paper zDepth={1} style={{height:100 +'%'}}>
+                            <Toolbar>
+                                <ToolbarGroup firstChild={true}>
+                                    <FlatButton label="< Back" primary={true} />
+                                </ToolbarGroup>
+                            </Toolbar>
                             <List>
-                                <ListItem primaryText="Back" />
-                                <ListItem leftAvatar={<Avatar icon={<FileFolder />} />} primaryText="All mail" />
-                                <ListItem leftAvatar={<Avatar icon={<FileFolder />} />} primaryText="Trash" />
-                                <ListItem leftAvatar={<Avatar icon={<FileFolder />} />} primaryText="Spam" />
-                                <ListItem leftAvatar={<Avatar icon={<FileFolder />} />} primaryText="Follow up" />
+                                <Subheader>Pages</Subheader>
+                                <ListItem innerDivStyle={{padding: '17px 16px 15px 57px'}} style={{fontSize: 14 + 'px', lineHeight: 14 + 'px'}} leftAvatar={<Avatar size={30} icon={<FileFolder />} />} primaryText="All mail" />
+                                <ListItem innerDivStyle={{padding: '17px 16px 15px 57px'}} style={{fontSize: 14 + 'px', lineHeight: 14 + 'px'}} leftAvatar={<Avatar size={30} icon={<FileFolder />} />} primaryText="Trash" />
+                                <ListItem innerDivStyle={{padding: '17px 16px 15px 57px'}} style={{fontSize: 14 + 'px', lineHeight: 14 + 'px'}} leftAvatar={<Avatar size={30} icon={<FileFolder />} />} primaryText="Spam" />
+                                <ListItem innerDivStyle={{padding: '17px 16px 15px 57px'}} style={{fontSize: 14 + 'px', lineHeight: 14 + 'px'}} leftAvatar={<Avatar size={30} icon={<FileFolder />} />} primaryText="Follow up" />
                             </List>
+                            <FloatingActionButton zDepth={1} mini={true} secondary={true} style={{float:'right', marginRight:20 + 'px'}}>
+                                <ContentAdd />
+                            </FloatingActionButton>
                         </Paper>
-                    </Col>
-                    <Col xs>
+                    </div>
+                    <Col xs={12} className="appcontent">
+                        <Grid fluid>
+                            <Row>
+                                <Col xs={6}>
+                                    <Paper zDepth={1}>
+                                        <TextField
+                                            hintText="Hint Text"
+                                            floatingLabelText="Fixed Floating Label Text"
+                                            floatingLabelFixed={true}
+                                        />
+                                    </Paper>
+                                </Col>
+                            </Row>
+                        </Grid>
                     </Col>
                 </Row>
             </Grid>
