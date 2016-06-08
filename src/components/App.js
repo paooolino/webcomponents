f7cc7a60cf7864e46338a5f67852eda9406a4024
Paolo Savoldi <paooolino@gmail.com>
2016-06-08 15:34:53 +0200

    App testing completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,31 @@ import LoginForm from './LoginForm';
 import ItemManager from './ItemManager';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    componentDidMount() {
+        this.props.handleGetLangInfos();
+    }
+    
     render() {
         const component = this.props.isAuthenticated ? <ItemManager /> : <LoginForm />;
-        const logout_button = this.props.isAuthenticated ? <button id="logout_button"></button> : '';
+        const logout_button = this.props.isAuthenticated ? <button onClick={this.props.handleLogout} id="logout_button"></button> : '';
         const fetching_overlay = this.props.isFetching ? <div id="fetching_overlay"></div> : '';
+        
         return (
-            <div>
+            <div id="appContainer">
                 {component}
                 <div id="appBar">
                     <div id="languageSelector">
-                        {logout_button}
+                        <ul>
+                            {this.props.languages.map( (language) => (
+                                <li onClick={ () => {this.props.handleChangeLanguage(language);} } key={language}>{language}</li>
+                            ))}
+                        </ul>
                     </div>
+                    {logout_button}
                 </div>
                 <div id="statusBar">
                 </div>
--- test/components/App_spec.js
+++ test/components/App_spec.js
@@ -1,47 +1,81 @@
 import expect from 'expect';
 import React from 'react';
-//import TestUtils from 'react-addons-test-utils';
-import { shallow } from 'enzyme';
+import jsdom from 'jsdom';
+import { shallow, mount } from 'enzyme';
 import { App } from '../../src/components/App';
 import { LoginForm } from '../../src/components/LoginForm';
 import { ItemManager } from '../../src/components/ItemManager';
 
-function setup(props) {
-    //let renderer = TestUtils.createRenderer();
-    //renderer.render(<App {...props} />);
-    //let output = renderer.getRenderOutput();
+const defaultProps = {
+    isAuthenticated: false,
+    isFetching: false,
+    handleLogout: expect.createSpy(),
+    handleGetLangInfos: expect.createSpy(),
+    handleChangeLanguage: expect.createSpy(),
+    languages: []
+};
     
-    const output = shallow(<App {...props} />);
+function setupDom() {
+    if (typeof document !== 'undefined') {
+        return;
+    }
+
+    global.document = jsdom.jsdom('<html><body></body></html>');
+    global.window = document.defaultView;
+    global.navigator = window.navigator;
+};
+
+function setupShallow(props) {
+    const actualProps = {...defaultProps, ...props};
+    const output = shallow(<App {...actualProps} />);
+    return output;
+}
+
+function setupMount(props, opts){
+    const actualProps = {...defaultProps, ...props};
+    const output = mount(<App {...actualProps} />, opts);
     return output;
 }
 
 describe('App component', () => {
-       
+    
+    setupDom();
+    
     describe('rendering', () => {
         
-        it('should render a wrapper div', () => {
-            const output = setup({isAuthenticated: false});
-            expect(output.is('div')).toBe(true);
+        it('should render a wrapper div having #appContainer as id', () => {
+            const output = setupShallow({isAuthenticated: false});
+            expect(output.find('#appContainer').length).toBe(1);
+            expect(output.find('#appContainer').is('div')).toBe(true);
         });
 
         it('should render the #appBar', () => {
-            const output = setup({isAuthenticated: false});
+            const output = setupShallow({isAuthenticated: false});
             expect(output.find('#appBar').length).toBe(1);
         });
         
         it('should render the #languageSelector in the #appBar', () => {
-            const output = setup({isAuthenticated: false});
+            const output = setupShallow({isAuthenticated: false});
             expect(output.find('#appBar #languageSelector').length).toBe(1);
         });
         
+        it('should render the available languages as unordered list in #languageSelector', () => {
+            const output = setupShallow({
+                isAuthenticated: false,
+                languages: ['it', 'en']
+            });
+            expect(output.find('#languageSelector ul').length).toBe(1);
+            expect(output.find('#languageSelector ul li').length).toBe(2);
+        });
+        
         it('should render the #statusBar', () => {
-            const output = setup({isAuthenticated: false});
+            const output = setupShallow({isAuthenticated: false});
             expect(output.find('#statusBar').length).toBe(1);
         });
         
         describe('When the user is not authenticated', () => {
             
-            const output = setup({isAuthenticated: false});
+            const output = setupShallow({isAuthenticated: false});
 
             it('should render the LoginForm component as first child', () => {
                 const firstChild = output.childAt(0);
@@ -56,7 +90,7 @@ describe('App component', () => {
         
         describe('When the user is authenticated', () => {
             
-            const output = setup({isAuthenticated: true});
+            const output = setupShallow({isAuthenticated: true});
             
             it('should render the ItemManager component as first child', () => {
                 const firstChild = output.childAt(0);
@@ -64,7 +98,7 @@ describe('App component', () => {
             });
             
             it('should render the #logout_button in the #appBar', () => {
-                expect(output.find('#logout_button').length).toBe(1);    
+                expect(output.find('#appBar #logout_button').length).toBe(1);    
                 expect(output.find('#logout_button').is('button')).toBe(true);    
             });
 
@@ -72,7 +106,7 @@ describe('App component', () => {
         
         describe('When the app is fetching', () => {
             
-            const output = setup({isFetching: true});
+            const output = setupShallow({isFetching: true});
             
             it('should render the #fetching_overlay', () => {
                 expect(output.find('#fetching_overlay').length).toBe(1);  
@@ -82,7 +116,7 @@ describe('App component', () => {
         
         describe('When the app is not fetching', () => {
             
-            const output = setup({isFetching: false});
+            const output = setupShallow({isFetching: false});
             
             it('should not render the #fetching_overlay', () => {
                 expect(output.find('#fetching_overlay').length).toBe(0);  
@@ -93,11 +127,35 @@ describe('App component', () => {
     });    
 
     describe('behaviour', () => {
-        it('should dispatch the logout action when the logout button is clicked');
-        it('should dispatch the getLangInfos actions when the component is mounted');
-        it('should dispatch the changeLanguage action when a new language is picked');
-        it('should dispatch the updateWindowHeight action when the component is mounted');
-        it('should dispatch the updateWindowHeight when the browser window is resized');
+        
+        it('should call handleGetLangInfos when the component is mounted', () => {
+            const handleGetLangInfos = expect.createSpy();
+            const output = setupMount({
+                handleGetLangInfos
+            });
+            expect(handleGetLangInfos.calls.length).toBe(1);            
+        });
+        
+        it('should call handleLogout when the logout button is clicked', () => {
+            const handleLogout = expect.createSpy();
+            const output = setupShallow({
+                isAuthenticated: true,
+                handleLogout
+            });
+            output.find('#logout_button').simulate('click');
+            expect(handleLogout.calls.length).toBe(1);
+        });
+        
+        it('should call handleChangeLanguage when a new language is clicked', () => {
+            const handleChangeLanguage = expect.createSpy();
+            const output = setupShallow({
+                handleChangeLanguage,
+                languages: ['it', 'en']
+            });
+            output.find('#languageSelector ul').childAt(0).simulate('click');
+            expect(handleChangeLanguage.calls.length).toBe(1);            
+        });
+
     });
     
 });
