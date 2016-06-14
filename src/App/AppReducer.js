74338f2f7d250cf5936ab281bc16dc2a67d78249
Paolo Savoldi <paooolino@gmail.com>
2016-06-14 16:13:50 +0200

    completing tests

---
+++ src/App/AppReducer.js
@@ -0,0 +1,63 @@
+import {
+    CHANGE_LANGUAGE, LOGOUT,
+    GET_LANGUAGES_REQUEST,
+    GET_LANGUAGES_FAILURE,
+    GET_LANGUAGES_SUCCESS
+} from './appActions.js';
+
+const initialState = {
+    nFetching: 0,
+    statusMessage: '',
+    languages: [],
+    selectedLanguage: '',
+    isLogged: false
+};
+
+export default function(state=initialState, action) {
+    switch (action.type) {
+        case GET_LANGUAGES_REQUEST:
+            return {
+                ...state,
+                nFetching: state.nFetching + 1,
+                statusMessage: 'Requesting languages...'
+            };
+            
+        case GET_LANGUAGES_FAILURE:
+            return {
+                ...state,
+                nFetching: state.nFetching - 1,
+                statusMessage: 'Error in requesting languages: ' + action.errorMessage
+            };
+            
+        case GET_LANGUAGES_SUCCESS:
+            return {
+                ...state,
+                nFetching: state.nFetching - 1,
+                statusMessage: 'Languages loaded.',
+                languages: action.languages
+            };
+            
+        case LOGOUT:
+            return {
+                ...state,
+                isLogged: false
+            };
+            
+        case CHANGE_LANGUAGE:
+            if( state.languages.indexOf(action.newLanguage) > -1 ) {
+                return {
+                    ...state,
+                    statusMessage: 'Language was changed to ' + action.newLanguage,
+                    selectedLanguage: action.newLanguage
+                }
+            } else {
+                return {
+                    ...state,
+                    statusMessage: 'Failed change to unavailable language ' + action.newLanguage
+                }
+            }
+            
+        default:
+            return state;
+    }
+}
--- test/App/AppReducer_spec.js
+++ test/App/AppReducer_spec.js
@@ -3,17 +3,96 @@ import reducer from '../../src/App/appReducer';
 import {changeLanguage, logout} from '../../src/App/AppActions';
 import {getLanguagesRequest, getLanguagesFailure, getLanguagesSuccess} from '../../src/App/AppActions';
 
+const initialState = {
+    nFetching: 0,
+    statusMessage: '',
+    languages: [],
+    selectedLanguage: '',
+    isLogged: false
+};
+
 describe('App reducer', () => {
     
-    xit('should handle getLanguagesRequest', () => {});
-    xit('should handle getLanguagesFailure', () => {});
-    xit('should handle getLanguagesSuccess', () => {});
-    xit('should handle logout', () => {});
+    it('should handle getLanguagesRequest', () => {
+        const state = {...initialState}
+        const nextState = reducer(state, getLanguagesRequest());
+        const expectedState = {
+            ...state,
+            nFetching: 1,
+            statusMessage: 'Requesting languages...'
+        };
+        expect(nextState).toEqual(expectedState);
+    });
+    it('should handle getLanguagesFailure', () => {
+        const state = {
+            ...initialState,
+            nFetching: 1,
+            statusMessage: 'Requesting languages...'
+        }
+        const errorMessage = 'An unspecified error occurred.';
+        const nextState = reducer(state, getLanguagesFailure(errorMessage));
+        const expectedState = {
+            ...state,
+            nFetching: 0,
+            statusMessage: 'Error in requesting languages: ' + errorMessage
+        };
+        expect(nextState).toEqual(expectedState);
+    });
+    it('should handle getLanguagesSuccess', () => {
+        const state = {
+            ...initialState,
+            nFetching: 1,
+            statusMessage: 'Requesting languages...'
+        }
+        const serverData = {
+            languages: ['it', 'en']
+        }
+        const nextState = reducer(state, getLanguagesSuccess(serverData));
+        const expectedState = {
+            ...state,
+            nFetching: 0,
+            statusMessage: 'Languages loaded.',
+            languages: serverData.languages
+        };
+        expect(nextState).toEqual(expectedState);
+    });
+    it('should handle logout', () => {
+        const state = {
+            ...initialState,
+            isLogged: true
+        }
+        const nextState = reducer(state, logout());
+        const expectedState = {
+            ...state,
+            isLogged: false
+        };
+        expect(nextState).toEqual(expectedState);        
+    });
     
     describe('changeLanguage', () => {
         
-        xit('should change a language if present in the available languages', () => {});
-        xit('should not change a language if not present in the available languages', () => {});
+        it('should change a language if present in the available languages', () => {
+            const state = {...initialState, languages: ['it', 'en']}
+            const newLanguage = 'en';
+            const nextState = reducer(state, changeLanguage(newLanguage));
+            const expectedState = {
+                ...state,
+                statusMessage: 'Language was changed to ' + newLanguage,
+                selectedLanguage: newLanguage
+            };        
+            expect(nextState).toEqual(expectedState);             
+        });
+        it('should not change a language if not present in the available languages', () => {
+            const state = {...initialState, languages: ['it', 'en'], selectedLanguage: ''}
+            const newLanguage = 'de';
+            const nextState = reducer(state, changeLanguage(newLanguage));
+            const expectedState = {
+                ...state,
+                statusMessage: 'Failed change to unavailable language ' + newLanguage,
+                selectedLanguage: ''
+            };        
+            expect(nextState).toEqual(expectedState);
+        });
         
     });
 
