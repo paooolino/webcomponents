6ecbf4b1d44b21be69461b31d0bec75f59b26ecb
Paolo Savoldi <paooolino@gmail.com>
2016-06-15 17:33:24 +0200

    actions tests

--- test/actions_spec.js
+++ test/actions_spec.js
@@ -7,23 +7,29 @@ describe('[actions_spec]', () => {
     xit('should export the correct Types', () => {
         const expectedTypes = [
             'CHANGE_LANGUAGE',
-            'LOGOUT',
+            'LOGOUT',      
+            'SELECT_ITEM',
+            
             'LOGIN_REQUEST',
             'LOGIN_FAILURE',
             'LOGIN_SUCCESS',
+            
             'REQUEST_LANGUAGES_REQUEST',
             'REQUEST_LANGUAGES_FAILURE',
             'REQUEST_LANGUAGES_SUCCESS',
+            
             'FETCH_ITEMS_REQUEST',
             'FETCH_ITEMS_FAILURE',
             'FETCH_ITEMS_SUCCESS',
-            'SELECT_ITEM',
+            
             'ADD_ITEM_REQUEST',
             'ADD_ITEM_FAILURE',
             'ADD_ITEM_SUCCESS',
+            
             'DELETE_ITEM_REQUEST',
             'DELETE_ITEM_FAILURE',
             'DELETE_ITEM_SUCCESS',
+            
             'SAVE_ITEM_FIELD_REQUEST',
             'SAVE_ITEM_FIELD_FAILURE',
             'SAVE_ITEM_FIELD_SUCCESS'
@@ -35,24 +41,33 @@ describe('[actions_spec]', () => {
         const expectedCreators = [
             'changeLanguage',
             'logout',
+            'selectItem',
+            
             'login',
+            'loginRequest',
+            'loginFailure',
+            'loginSuccess',
+            
             'requestLanguages',
             'requestLanguagesRequest',
             'requestLanguagesFailure',
             'requestLanguagesSuccess',
+            
             'fetchItems',
             'fetchItemsRequest',
             'fetchItemsFailure',
             'fetchItemsSuccess',
-            'selectItem',
+            
             'addItem',
             'addItemRequest',
             'addItemFailure',
             'addItemSuccess',
+            
             'deleteItem',
             'deleteItemRequest',
             'deleteItemFailure',
             'deleteItemSuccess',
+            
             'saveItemField',
             'saveItemFieldRequest',
             'saveItemFieldFailure',
@@ -63,14 +78,48 @@ describe('[actions_spec]', () => {
     
     describe('Sync action creators', () => {
         
-        xit('should create an action to change language', () => {});
-        xit('should create an action to logout', () => {});
+        xit('should create an action to change language', () => {
+            expect(creators.changeLanguage('it')).toEqual({
+                type: types.CHANGE_LANGUAGE,
+                newLanguage: 'it'
+            });
+        });
         
-        xit('should create an action to request a login', () => {});
-        xit('should create an action to fail a login', () => {});
-        xit('should create an action to success a login', () => {});
+        xit('should create an action to logout', () => {
+            expect(creators.logout()).toEqual({
+                type: types.LOGOUT
+            });
+        });
         
-        xit('should create an action to select an item', () => {});
+        xit('should create an action to request a login', () => {
+            const user = 'whatever_username';
+            const pass = 'whatever_password';
+            expect(creators.loginRequest(user, pass)).toEqual({
+                type: types.LOGIN_REQUEST,
+                user,
+                pass
+            });
+        });
+        xit('should create an action to fail a login', () => {
+            const errorMessage = 'whatever error message';
+            expect(creators.loginFailure()).toEqual({
+                type: types.LOGIN_FAILURE,
+                errorMessage
+            });
+        });
+        xit('should create an action to success a login', () => {
+            expect(creators.loginSuccess()).toEqual({
+                type: types.LOGIN_SUCCESS
+            });
+        });
+        
+        xit('should create an action to select an item', () => {
+            const item_id = 3;
+            expect(creators.selectItem(item_id)).toEqual({
+                type: types.SELECT_ITEM,
+                selectedItem: item_id
+            });
+        });
         
         xit('should create an action to request a language request', () => {});
         xit('should create an action to fail a language request', () => {});
