f8fe7dab885a4517592ffa4bad6a0894f309e9ed
Paolo Savoldi <paooolino@gmail.com>
2016-06-29 11:27:23 +0200

    automated creation tests

--- test/actionCreators_spec.js
+++ test/actionCreators_spec.js
@@ -11,9 +11,6 @@ import nock from 'nock';
     internal imports
 */
 
-const ENDPOINT_HOST = 'http://127.0.0.1';
-const ENDPOINT_PATH = '/webcomponents/server/src/endpoint.php';
-
 import * as creators from '../src/actionCreators';
 import * as types from '../src/actionTypes';
 
@@ -21,6 +18,8 @@ import * as types from '../src/actionTypes';
     setup
 */
 
+const ENDPOINT_HOST = 'http://127.0.0.1';
+const ENDPOINT_PATH = '/webcomponents/server/src/endpoint.php';
 const middlewares = [thunkMiddleware];
 const mockStore = configureMockStore(middlewares);
 
@@ -29,107 +28,103 @@ const mockStore = configureMockStore(middlewares);
 */
 
 describe('[actionCreators_spec]', () => {
-    
-    describe('login', () => {
-        
+
+    describe('LOGIN', () => {
+
         it('creates the LOGIN_REQUEST action', () => {
             const action = creators.loginRequest();
             expect(action).toEqual({
                 type: types.LOGIN_REQUEST
-            }); 
+            });
         });
-        
+
         it('creates the LOGIN_FAILURE action', () => {
-            const serverErrorMessage = 'Whatever error message';
+            const serverErrorMessage = "Whatever error message";
             const action = creators.loginFailure(serverErrorMessage);
             expect(action).toEqual({
                 type: types.LOGIN_FAILURE,
-                errorMessage: 'Login failure: Whatever error message'
-            }); 
+                errorMessage: "Whatever error message"
+            });
         });
 
         it('creates the LOGIN_SUCCESS action', () => {
-            const serverData = {
-                authCode: 'whatever_auth_code'
-            };
+            const serverData = {"authCode":"whatever_auth_code"};
             const action = creators.loginSuccess(serverData);
             expect(action).toEqual({
                 type: types.LOGIN_SUCCESS,
-                authCode: 'whatever_auth_code'
-            }); 
+                authCode: "whatever_auth_code"
+            });
         });
-        
-        it('creates the LOGIN action which passes the correct parameters to the server', () => {
+
+        it('creates the async LOGIN action which passes the correct parameters to the server', () => {
             let passedData;
             nock(ENDPOINT_HOST).post(ENDPOINT_PATH)
                 .reply(200, function(uri, requestBody){
                     passedData = requestBody;
                 });
-               
+
             const store = mockStore({});
 
-            return store.dispatch(creators.login('admin', 'admin'))
+            const username = "admin";
+            const password = "admin123";
+            return store.dispatch(creators.login(username, password))
                 .then(() => {
                     expect(passedData.action).toBe('login');
-                    expect(passedData.username).toBe('admin');
-                    expect(passedData.password).toBe('admin');
+                    expect(passedData.username).toBe("admin");
+                    expect(passedData.password).toBe("admin123");
                 });
         });
-        
-        it('creates the CHANGE_USERNAME action', () => {
-            const action = creators.changeUsername('whatever_username');
-            expect(action).toEqual({
-                type: types.CHANGE_USERNAME,
-                newValue: 'whatever_username'
-            }); 
-        });
-        
-        it('creates the CHANGE_PASSWORD action', () => {
-            const action = creators.changePassword('whatever_password');
-            expect(action).toEqual({
-                type: types.CHANGE_PASSWORD,
-                newValue: 'whatever_password'
-            }); 
-        });
-        
+
     });
-    
-    describe('getLanguages', () => {
-        
+
+    it('creates the CHANGE_FIELD_USERNAME action', () => {
+        const action = creators.changeFieldUsername();
+        expect(action).toEqual({
+            type: types.CHANGE_FIELD_USERNAME
+        });
+    });
+
+    it('creates the CHANGE_FIELD_PASSWORD action', () => {
+        const action = creators.changeFieldPassword();
+        expect(action).toEqual({
+            type: types.CHANGE_FIELD_PASSWORD
+        });
+    });
+
+    describe('GET_LANGUAGES', () => {
+
         it('creates the GET_LANGUAGES_REQUEST action', () => {
             const action = creators.getLanguagesRequest();
             expect(action).toEqual({
                 type: types.GET_LANGUAGES_REQUEST
-            }); 
+            });
         });
-        
+
         it('creates the GET_LANGUAGES_FAILURE action', () => {
-            const serverErrorMessage = 'Whatever error message';
+            const serverErrorMessage = "Whatever error message";
             const action = creators.getLanguagesFailure(serverErrorMessage);
             expect(action).toEqual({
                 type: types.GET_LANGUAGES_FAILURE,
-                errorMessage: 'Get languages failure: Whatever error message'
-            }); 
+                errorMessage: "Whatever error message"
+            });
         });
 
         it('creates the GET_LANGUAGES_SUCCESS action', () => {
-            const serverData = {
-                languages: ['it', 'en']
-            };
+            const serverData = {"languages":["it","en"]};
             const action = creators.getLanguagesSuccess(serverData);
             expect(action).toEqual({
                 type: types.GET_LANGUAGES_SUCCESS,
-                languages: ['it', 'en']
-            }); 
+                languages: ["it","en"]
+            });
         });
-        
-        it('creates the GET_LANGUAGES action which passes the correct parameters to the server', () => {
+
+        it('creates the async GET_LANGUAGES action which passes the correct parameters to the server', () => {
             let passedData;
             nock(ENDPOINT_HOST).post(ENDPOINT_PATH)
                 .reply(200, function(uri, requestBody){
                     passedData = requestBody;
                 });
-               
+
             const store = mockStore({});
 
             return store.dispatch(creators.getLanguages())
@@ -137,5 +132,15 @@ describe('[actionCreators_spec]', () => {
                     expect(passedData.action).toBe('getLanguages');
                 });
         });
+
     });
+
+    it('creates the SELECT_LANGUAGE action', () => {
+        const action = creators.selectLanguage();
+        expect(action).toEqual({
+            type: types.SELECT_LANGUAGE
+        });
+    });
+
 });
+
--- test_generator.php
+++ test_generator.php
@@ -3,8 +3,12 @@ $actionTypes = array(
     "LOGIN" => array(
         "type" => "async",
         "creator" => "login",
+        "dataToPass" => array(
+            "username" => "admin",
+            "password" => "admin123"
+        ),
         "serverData" => array(
-            "authCode" => "'whatever_auth_code'"
+            "authCode" => "whatever_auth_code"
         )
     ),
     "CHANGE_FIELD_USERNAME" => array(
@@ -17,7 +21,7 @@ $actionTypes = array(
         "type" => "async",
         "creator" => "getLanguages",
         "serverData" => array(
-            "languages" => "['it', 'en']"
+            "languages" => array("it", "en")
         )
     ),
     "SELECT_LANGUAGE" => array(
@@ -59,20 +63,23 @@ file_put_contents("test/actionTypes_spec.js", $code);
 */
 
 $code = "";
+$code .= comment(0, "external imports");
 $code .= import("expect", "expect");
 $code .= import("configureMockStore", "redux-mock-store");
 $code .= import("thunkMiddleware", "redux-thunk");
 $code .= import("nock", "nock");
 $code .= emptyrow();
+$code .= comment(0, "internal imports");
 $code .= import("* as creators", "../src/actionCreators");
 $code .= import("* as types", "../src/actionTypes");
 $code .= emptyrow();
+$code .= comment(0, "setup");
+$code .= "const ENDPOINT_HOST = 'http://127.0.0.1';\r\n";
+$code .= "const ENDPOINT_PATH = '/webcomponents/server/src/endpoint.php';\r\n";
 $code .= "const middlewares = [thunkMiddleware];\r\n";
 $code .= "const mockStore = configureMockStore(middlewares);\r\n";
 $code .= emptyrow();
-$code .= "const ENDPOINT_HOST = 'http://127.0.0.1';\r\n";
-$code .= "const ENDPOINT_PATH = '/webcomponents/server/src/endpoint.php';\r\n";
-$code .= emptyrow();
+$code .= comment(0, "tests");
 $code .= describeBegin(0, "[actionCreators_spec]");
 foreach($actionTypes as $actionName => $actionConfig) {
     if( isset($actionConfig["type"]) && $actionConfig["type"] == "async" ) {
@@ -82,25 +89,63 @@ foreach($actionTypes as $actionName => $actionConfig) {
         $code .= testActionCreator(2, $actionName . "_REQUEST", $actionConfig["creator"] . "Request");
         $code .= testActionCreator(2, $actionName . "_FAILURE", $actionConfig["creator"] . "Failure", array(
             "inputs" => array(
-                "serverErrorMessage" => "'Whatever error message'"
+                "serverErrorMessage" => "Whatever error message"
             ),
             "outputs" => array(
-                "errorMessage" => "'Login failure: Whatever error message'"
+                "errorMessage" => "Whatever error message"
             )
         ));
+        $outputs = array();
+        foreach($actionConfig["serverData"] as $k => $v) {
+            $outputs[$k] = $v;
+        }
         $code .= testActionCreator(2, $actionName . "_SUCCESS", $actionConfig["creator"] . "Success", array(
-            "inputs" => $actionConfig["serverData"],
-            "outputs" => $actionConfig["serverData"]
+            "inputs" => array(
+                "serverData" => $actionConfig["serverData"]
+            ),
+            "outputs" => $outputs
         ));
         
+        // async action creator
+        $code .= itBegin(2, "creates the async $actionName action which passes the correct parameters to the server");
+        $code .= rawrow(3, "let passedData;");
+        $code .= rawrow(3, "nock(ENDPOINT_HOST).post(ENDPOINT_PATH)");
+        $code .= rawrow(4, ".reply(200, function(uri, requestBody){");
+        $code .= rawrow(5, "passedData = requestBody;");
+        $code .= rawrow(4, "});");
+        $code .= emptyrow();
+        $code .= rawrow(3, "const store = mockStore({});");
+        $code .= emptyrow();
+        $inputsList = "";
+        if( isset($actionConfig["dataToPass"]) ) {
+            $inputsList = implode(", ", array_keys($actionConfig["dataToPass"]));
+        }
+        if( isset($actionConfig["dataToPass"]) ) {
+            foreach($actionConfig["dataToPass"] as $k => $v) {
+                $code .= rawrow(3, "const $k = " . json_encode($v) . ";");
+            }
+        }
+        $code .= rawrow(3, "return store.dispatch(creators.{$actionConfig["creator"]}($inputsList))");
+        $code .= rawrow(4, ".then(() => {");
+        $code .= rawrow(5, "expect(passedData.action).toBe('{$actionConfig["creator"]}');");
+        if( isset($actionConfig["dataToPass"]) ) {
+            foreach($actionConfig["dataToPass"] as $k => $v) {
+                $code .= rawrow(5, "expect(passedData.$k).toBe(" . json_encode($v) . ");");
+            }
+        }
+        $code .= rawrow(4, "});");
+        $code .= itEnd(2);
+        
         $code .= describeEnd(1);
+        
     } else {
         // sync action
         $code .= testActionCreator(1, $actionName, $actionConfig["creator"]);
     }
 }
 $code .= describeEnd(0);
-echo $code;
+
+file_put_contents("test/actionCreators_spec.js", $code);
 
 
 
@@ -146,16 +191,37 @@ function rawrow($level, $code) {
 function testActionCreator($level, $actionName, $creator, $opts=array()) {
     $code = "";
     $code .= itBegin($level, "creates the $actionName action");
+    $inputsList = "";
     if( isset($opts["inputs"]) ) {
-        $attrs = array();
-        foreach($opts["inputs"] as $k => $v) {
-            array_push($attrs, "$k: $v");
-        }
-        $code .= rawrow($level+1, "const serverData = {" . implode(",", $attrs) . "};");
+        $inputsList = implode(", ", array_keys($opts["inputs"]));
     }
-    $code .= rawrow($level+1, "const action = creators.$creator();");
-    $code .= expect($level+1, "action", "toEqual", "{type: types.$actionName}");
+    if( isset($opts["inputs"]) ) {
+        foreach($opts["inputs"] as $k => $v) {
+            $code .= rawrow($level+1, "const $k = " . json_encode($v) . ";");
+        }
+    }
+    $outputsArr = array(str_repeat(" ", ($level+2)*4) . "type: types.$actionName");
+    if( isset($opts["outputs"]) ) {
+        foreach($opts["outputs"] as $k => $v) {
+            array_push($outputsArr, str_repeat(" ", ($level+2)*4) . "$k: " . json_encode($v));
+        }
+    }
+    $expectedAction = "";
+    $expectedAction .= "{\r\n";
+    $expectedAction .= implode(",\r\n", $outputsArr) . "\r\n";
+    $expectedAction .= str_repeat(" ", ($level+1)*4) . "}";
+    
+    $code .= rawrow($level+1, "const action = creators.$creator({$inputsList});");
+    $code .= expect($level+1, "action", "toEqual", $expectedAction);
     $code .= itEnd($level);
     
+    return $code;
+}
+
+function comment($level, $s) {
+    $code = "";
+    $code .= str_repeat(" ", $level*4) . "/*\r\n";
+    $code .= str_repeat(" ", ($level+1)*4) . $s . "\r\n";
+    $code .= str_repeat(" ", $level*4) . "*/\r\n\r\n";
     return $code;
 }
